f0d79f6d6a358ef164c4033ef1b9f516c10d600f
Pablo <pablogdpomar@gmail.com>
2026-09-01 14:05:37 +0200

    Version 1.1.0

--- airtouch/version.py
+++ airtouch/version.py
@@ -6,7 +6,7 @@ escrito a mano en dos sitios distintos.
 """
 from __future__ import annotations
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 #: Repositorio publico del que se descargan las actualizaciones.
 GITHUB_OWNER = "Pablodev-star"
